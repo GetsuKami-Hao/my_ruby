7b935202180d21bc0d949e2d40e057b4a3cb7c64
yirolhao <yirolhao@gmail.com>
2016-08-25 16:25:44 +0800

    改变数组内容

--- 2016/august/changeArray.rb
+++ 2016/august/changeArray.rb
@@ -3,3 +3,6 @@ alpha[2..4] = ["2","3","4"]
 p alpha
 alpha[5...6] = [".",","] #three or two is ok,others is error.
 p alpha
+a = ["a","b","c","d","e","f","g"];
+a[3,0] = ["4","5"] #在下表为3的地方连续插入
+p a
