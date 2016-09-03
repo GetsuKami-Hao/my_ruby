a7484d10bac3e046837960040343b9aa85f1adb6
yirolhao <yirolhao@gmail.com>
2016-09-03 17:22:01 +0800

    处理字符串

---
+++ 2016/september/get_string_length.rb
@@ -0,0 +1,8 @@
+p "just another ruby hacher.".length
+p "just another ruby hacher.".size
+
+#It applies only to english in previous editions.
+
+p '面向对象编程语言'.length
+
+p '面向对象编程语言'.split(//u).length
