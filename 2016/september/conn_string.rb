142273c98f480fd086e99527d6360b61444ef0fd
yirolhao <yirolhao@gmail.com>
2016-09-03 18:49:33 +0800

    字符串的连接

---
+++ 2016/september/conn_string.rb
@@ -0,0 +1,15 @@
+hello = "hello,"
+world = "world!"
+
+str = hello + world;
+# “+” symbol create a new string.
+p str;
+
+hello << world
+
+p hello
+
+hello.concat(world)
+p hello
+
+#“<<” symbol  and “concat” function , existing string.
