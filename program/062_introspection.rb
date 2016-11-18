3980ea35b334542da371e3e5754b6d9cba1ea253
yirolhao <yirolhao@gmail.com>
2016-11-18 21:29:01 +0800

    rename dir

---
+++ program/062_introspection.rb
@@ -0,0 +1,16 @@
+class Greeting
+
+	def initialize(text)
+		@text = text
+	end
+
+	def welcome
+		@text
+	end
+end
+
+my_object = Greeting.new("hello")
+puts my_object.class
+
+p my_object.class.instance_methods(false) #don't include inheritance instance method
+p my_object.instance_variables
