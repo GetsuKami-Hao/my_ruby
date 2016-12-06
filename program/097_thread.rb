0ba41cbae5eba1fd4d6d99c66e0fc8347a07ec27
yirolhao <yirolhao@gmail.com>
2016-12-06 23:13:01 +0800

    thread

---
+++ program/097_thread.rb
@@ -0,0 +1,24 @@
+def thread1
+	i = 0
+	while i <= 2
+		puts "thread1 at: #{Time.now}"
+		sleep(2)
+		i += 1;
+	end
+end
+
+def thread2
+	j = 0
+	while j <= 2
+		puts "thread2 at #{Time.now}"
+		sleep(2)
+		j += 1
+	end
+end
+
+puts "started at #{Time.now}"
+t1 = Thread.new { thread1() }
+t2 = Thread.new { thread2() }
+t1.join()
+t2.join()
+puts "End at #{Time.now}"
