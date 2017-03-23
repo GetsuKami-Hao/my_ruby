f70d4369f1d24c9a7527b8a9e57115af4540f8ae
yirolhao <yirolhao@gmail.com>
2017-03-23 15:33:56 +0800

    格式化

--- program/058_method_missing.rb
+++ program/058_method_missing.rb
@@ -1,24 +1,23 @@
 class A
-	@@attributes = {}
+  @@attributes = {}
 
-	class << self
-		def method_missing method_name, *params
-			method_name = method_name.to_s
-			# p method_name # => name=
+  class << self
+    def method_missing(method_name, *params)
+      method_name = method_name.to_s
+      # p method_name # => name=
 			# p params
-			if method_name =~ /=$/
-				@@attributes[(method_name.sub('=',''))] = params.first
-				# p params.first
-			else
-				p method_name
-				@@attributes[method_name]
-			end
-		end
+    if method_name =~ /=$/
+      @@attributes[(method_name.sub('=',''))] = params.first
+			# p params.first
+      else
+        p method_name
+        @@attributes[method_name]
+      end
+    end
 
-		def to_s
-			@@attributes
-		end
+    def to_s
+      @@attributes
+    end
 	end
 
 end
-
